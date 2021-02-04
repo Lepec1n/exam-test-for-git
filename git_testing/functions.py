9ed68ec18c9e6f4e91519da5e5a7437ab05d3032
kSahatova <kseniya.shtv.98@gmail.com>
2021-02-05 00:53:41 +0300

    inc_and_sum() was modyfied, 'inc' argument was added

--- git_testing/functions.py
+++ git_testing/functions.py
@@ -10,18 +10,14 @@ def world2():
 def func1():
     var1 = 5
     var2 = 6
-    var1 = var1 + 1
-    var2 = var2 + 1
-    print(inc_and_sum(var1,var2))
+    print(inc_and_sum(var1,var2, 2))
 
 def func2():
     var1 = 3
     var2 = 4
-    var1 = var1 + 1
-    var2 = var2 + 1
-    print(inc_and_sum(var1,var2))
+    print(inc_and_sum(var1,var2, 2))
 
-def inc_and_sum(var1, var2):
-    var1 = var1 + 1
-    var2 = var2 + 1
+def inc_and_sum(var1, var2, inc):
+    var1 = var1 + inc
+    var2 = var2 + inc
     return var1 + var2
