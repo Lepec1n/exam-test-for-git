947bdb4491dba961d858faf0bbebebdbb73b36bb
kSahatova <kseniya.shtv.98@gmail.com>
2021-02-04 14:08:12 +0300

    world1() and world2() were saved in functions.py

--- git_testing/functions.py
+++ git_testing/functions.py
@@ -1,6 +1,8 @@
 author = 'Kseniya'
 world = 'World'
 
-def world():
+def world1():
     print(f"Hello, {world}! This is {author}!", file=open('../result.txt',"w"))
 
+def world2():
+    print(f"Hello, World! This is {author}!", file=open('../result.txt',"w"))
