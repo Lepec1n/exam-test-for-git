7c97140b12cda586f9df73496ac4882783a52d40
kSahatova <kseniya.shtv.98@gmail.com>
2021-02-04 13:46:19 +0300

    In world() key 'a' was changed to 'w' in functions.py to overwrite file result.txt

--- git_testing/functions.py
+++ git_testing/functions.py
@@ -2,5 +2,5 @@ author = 'Stas'
 
 
 def world():
-    print(f"Hello, World! This is {author}!", file=open('../result.txt',"a"))
+    print(f"Hello, World! This is {author}!", file=open('../result.txt',"w"))
 
